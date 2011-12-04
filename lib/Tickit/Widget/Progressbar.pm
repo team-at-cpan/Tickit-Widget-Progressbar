cfa15e631c1484f154b05ecd088ade8971aeea7d
Tom Molesworth <tom@perlsite.co.uk>
2011-12-04 22:28:04 +0000

    Update documentation
    git-svn-id: http://entitymodel.com/repo/trunk/Tickit/Tickit-Widget-Progressbar@192 c09ddf91-2ef0-45e8-8c68-47ccf8e3121d

--- lib/Tickit/Widget/Progressbar.pm
+++ lib/Tickit/Widget/Progressbar.pm
@@ -1,5 +1,5 @@
 package Tickit::Widget::Progressbar;
-# ABSTRACT: 
+# ABSTRACT: horizontal/vertical progress bars for Tickit
 use strict;
 use warnings FATAL => 'all';
 use parent qw(Tickit::Widget);
@@ -13,9 +13,11 @@ Tickit::Widget::Progressbar - simple progressbar implementation for Tickit
 
 =head1 SYNOPSIS
 
- my $bar = Tickit::Widget::Progressbar->new(
+ use Tickit::Widget::Progressbar::Horizontal;
+ my $bar = Tickit::Widget::Progressbar::Horizontal->new(
  	completion	=> 0.00,
  );
+ $bar->completion($_ / 100.0) for 0..100;
 
 =head1 METHODS
 
@@ -23,6 +25,27 @@ Tickit::Widget::Progressbar - simple progressbar implementation for Tickit
 
 sub lines { 1 }
 sub cols { 1 }
+
+=head2 new
+
+Instantiate a new L<Tickit::Widget::Progressbar> object. Takes the following named parameters:
+
+=over 4
+
+=item * completion - a value from 0.0 to 1.0 indicating progress
+
+=item * orientation - 'vertical' or 'horizontal'
+
+=item * direction - whether progress goes forwards (left to right, bottom to top) or backwards
+(right to left, top to bottom).
+
+=back
+
+Note that this is a base class, and the appropriate L<Tickit::Widget::Progressbar::Horizontal>
+or L<Tickit::Widget::Progressbar::Vertical> subclass should be used when instantiating a real
+widget.
+
+=cut
 
 sub new {
 	my $class = shift;
@@ -41,6 +64,13 @@ sub orientation { 'horizontal' }
 sub style { 'boxchar' }
 sub direction { shift->{direction} }
 
+=head2 completion
+
+Accessor for the current progress bar completion state - call this with a float value from 0.00..1.00
+to set completion and re-render.
+
+=cut
+
 sub completion {
 	my $self = shift;
 	if(@_) {
@@ -56,6 +86,8 @@ sub completion {
 __END__
 
 =head1 SEE ALSO
+
+L<Tickit>
 
 =head1 AUTHOR
 
